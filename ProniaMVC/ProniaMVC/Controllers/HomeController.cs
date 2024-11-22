06f8dd413a04f3ff897ac08b9a7ea19a9b1d8890
SabirGuliyev <sabir.guliyev@code.edu.az>
2024-11-22 14:14:12 +0400

    Initial commit

---
+++ ProniaMVC/ProniaMVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProniaMVC.DAL;
+using ProniaMVC.Models;
+using ProniaMVC.ViewModels;
+
+namespace ProniaMVC.Controllers
+{
+
+    //DI Dependency Injection
+    //IOC/ DIP  Inverse of control  / Dependency Inversion Principle
+    //IOC Container / DI Container
+
+    public class HomeController:Controller
+    {
+        private readonly AppDbContext _context;
+
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+       
+        }
+        public IActionResult Index()
+        {
+
+            HomeVM homeVM = new HomeVM {
+            Slides= _context.Slides.OrderBy(s => s.Order).Take(2).ToList(),
+            Products=_context.Products.Include(p => p.ProductImages).ToList()
+            };
+
+            return View(homeVM);
+        }
+
+    }
+}
